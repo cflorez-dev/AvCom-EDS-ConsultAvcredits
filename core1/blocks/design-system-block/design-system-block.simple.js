b2714936036fba63fee3c42b692b42abab02c051
Cesar Florez Forero <2022661@TCS.com>
2026-09-10 10:11:42 -0500

    Eliminar carpeta temporal core1

--- core1/blocks/design-system-block/design-system-block.simple.js
+++
@@ -1,4 +0,0 @@
-// Versión simplificada para debugging
-export default function decorate(block) {
-  block.innerHTML = '<div><h1>Design System - Versión Simple (Funciona)</h1><p>Si ves esto, el bloque carga correctamente.</p></div>';
-}
