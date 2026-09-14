6bf9e8a2800af2e4617dda2a22aad56bc1fda0a4
Cesar Florez Forero <2022661@TCS.com>
2026-09-14 09:11:34 -0500

    Visualización formulario con validaciones

--- blocks/form-header-banner/form-header-banner.js
+++ blocks/form-header-banner/form-header-banner.js
@@ -1,6 +1,9 @@
-// blocks/form-header-banner/form-header-banner.js
+import { h, render } from '@dropins/tools/preact.js';
+import htm from 'htm';
 import { default as coreDecorate } from '/core/blocks/form-header-banner/form-header-banner.js';
+import { ConsultAvCreditsForm } from '/design-system/organisms/forms/consult-av-credits-form/consult-av-credits-form.js';
 
+const html = htm.bind(h);
 /**
  * Maps block options from HTML according to the model defined in _component-models.json
  
@@ -11,6 +14,20 @@ import { default as coreDecorate } from '/core/blocks/form-header-banner/form-he
  * Decorates the Form Header Banner block
  * @param {Element} block The form-header-banner block element
  */
-export default function decorate(block) {
+export default async function decorate(block) {
   await coreDecorate(block);
+  const coreFormElement = block.querySelector('form');
+
+  if (!coreFormElement) {
+    return;
+  }
+
+  const formContainer = coreFormElement.parentElement;
+
+  formContainer.innerHTML = '';
+
+  render(
+    html`<${ConsultAvCreditsForm} />`,
+    formContainer
+  );
 }
